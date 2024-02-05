155897dd5aa47ab8e029b34c24be537899d4e774
RWNR <pacheung1@s.sfusd.edu>
2024-02-05 13:54:19 -0800

    p2 finished

--- player.gd
+++ player.gd
@@ -30,10 +30,10 @@ func apply_gravity(delta):
 		velocity.y += gravity * delta
 
 func handle_jump():
-	if is_on_floor(): 
+	if is_on_floor() or coyote_jump_timer.time_left > 0.0: 
 		if Input.is_action_just_pressed("ui_up"):
 			velocity.y = JUMP_VELOCITY
-	else:
+	if not is_on_floor():
 		if Input.is_action_just_released("ui_up") and velocity.y < JUMP_VELOCITY / 2:
 			velocity.y = JUMP_VELOCITY / 2
 
