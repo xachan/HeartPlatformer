de3a86937a8283312df0226a4cec5196184cf6cb
xachan <121971475+xachan@users.noreply.github.com>
2024-02-05 13:48:21 -0800

    p2

--- player.gd
+++ player.gd
@@ -18,18 +18,23 @@ func _physics_process(delta):
 
 	handle_acceleration(input_axis, delta)
 	apply_friction(input_axis, delta)
+	update_annimations(input_axis)
+	var was_on_floor = is_on_floor()
 	move_and_slide()
+	var just_left_ledge = was_on_floor and not is_on_floor() and velocity.y >= 0
+	if just_left_ledge:
+		coyote_jump_timer.start()
 
 func apply_gravity(delta):
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
 func handle_jump():
-	if is_on_floor():
-		if Input.is_action_just_pressed("ui_accept"):
+	if is_on_floor(): 
+		if Input.is_action_just_pressed("ui_up"):
 			velocity.y = JUMP_VELOCITY
 	else:
-		if Input.is_action_just_released("ui_accept") and velocity.y < JUMP_VELOCITY / 2:
+		if Input.is_action_just_released("ui_up") and velocity.y < JUMP_VELOCITY / 2:
 			velocity.y = JUMP_VELOCITY / 2
 
 func handle_acceleration(input_axis, delta):
@@ -39,3 +44,13 @@ func handle_acceleration(input_axis, delta):
 func apply_friction(input_axis, delta):
 	if input_axis == 0:
 		velocity.x = move_toward(velocity.x, 0, FRICTION * delta)
+
+func update_annimations(input_axis):
+	if input_axis != 0:
+		animated_sprite_2d.play("run")
+		animated_sprite_2d.flip_h = (input_axis < 0)
+	else:
+		animated_sprite_2d.play("idle")
+	if not is_on_floor():
+		animated_sprite_2d.play("jump")
+	
