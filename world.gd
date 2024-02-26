174daa4938f7ea08c90147029af10f365bd6150c
xachan <121971475+xachan@users.noreply.github.com>
2024-02-26 00:28:57 -0800

    6 done

--- world.gd
+++ world.gd
@@ -1,14 +1,15 @@
 extends Node2D
 
-@onready var collision_polygon_2d = $StaticBody2D/CollisionPolygon2D
-@onready var polygon_2d = $StaticBody2D/CollisionPolygon2D/Polygon2D
+@onready var next_level: PackedScene
+
 @onready var level_completed = $CanvasLayer/LevelCompleted
 
 func _ready():
 	RenderingServer.set_default_clear_color(Color.MIDNIGHT_BLUE)
-	polygon_2d.polygon = collision_polygon_2d.polygon
 	Events.level_completed.connect(show_level_completed)
 	
 func show_level_completed():
 	level_completed.show()
+	if not next_level is PackedScene return
+	get_tree().change_scene_to_packed(next_level)
 	get_tree().paused = true
